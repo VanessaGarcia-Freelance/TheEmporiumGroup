1dbcdb0adab7081467eb60e1a35271df328a6b8e
Vanessa Garcia <me@vanessa-lyn.com>
2015-11-05 18:12:15 -0800

    adding option to import svgs into the media library.

--- wp-content/themes/Emporiumtheme/functions.php
+++ wp-content/themes/Emporiumtheme/functions.php
@@ -1,7 +1,16 @@
 <?php // Opening PHP tag - nothing should be before this, not even whitespace
 
+
+//adding styles from parent the
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+
+//using SVGs inside the media library
+function svg_mime_types( $mimes ){
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;}
+add_filter( 'upload_mimes', 'svg_mime_types' );
